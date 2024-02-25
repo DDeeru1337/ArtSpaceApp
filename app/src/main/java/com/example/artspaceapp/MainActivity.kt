bc85ea1041117dbe23c6ce2cc99b9f0848f87577
DDeer <ddeeru1337@gmail.com>
2024-02-25 17:55:06 +0300

    Sceleton

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,7 +21,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,21 +33,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceAppTheme {
-                    ArtSpaceLayout()
+                    ArtSpaceLayout(
+
+                    )
             }
         }
     }
 }
 
 @Composable
-fun TextWithWhiteBorders(
-    text: String
+fun TextWithBorders(
+    text: String,
+    fontFamily: FontFamily,
+    backColor: Color,
+    modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = 
+        modifier = modifier
+            .background(backColor)
+            .border(4.dp, Color.Black)
     ) {
         Text(
-            text = text
+            text = text,
+            color = Color.Black,
+            fontFamily = fontFamily,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(8.dp)
         )
     }
 }
@@ -61,22 +80,43 @@ fun ArtSpaceLayout() {
             alpha = 0.8f
         )
         Column(
-
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
         ) {
-            Text(
+            TextWithBorders(
                 text = "Vincent van Gogh",
-                modifier = Modifier
-                    .background(Color.White)
-            )
+                fontFamily = FontFamily(Font(R.font.permanent_marker)),
+                backColor = Color.White
+                )
             Box(
                 modifier = Modifier
+                    .padding(24.dp)
+                    .border(12.dp, Color.DarkGray)
                     .background(Color.White)
+                    .padding(16.dp)
+
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.vincent_van_gogh),
                     contentDescription = null
                 )
             }
+            TextWithBorders(
+                text = "Self-Portrait, 1887",
+                fontFamily = FontFamily(Font(R.font.permanent_marker)),
+                backColor = Color.White
+            )
+            TextWithBorders(
+                text = "Van Gogh created more than 43 self-portraits between 1885 and 1889. " +
+                        "They were usually completed in series, such as those painted in Paris in mid-1887, and continued until shortly before his death. " +
+                        "Generally the portraits were studies, created during periods when he was reluctant to mix with others, or when he lacked models, and so painted himself.",
+                fontFamily = FontFamily(Font(R.font.oswald_variable)),
+                backColor = Color.LightGray,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            )
         }
     }
 }
