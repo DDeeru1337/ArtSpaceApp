27a232844a850c8a692a4c54095441de881dce1d
DDeer <ddeeru1337@gmail.com>
2024-02-25 20:25:22 +0300

    Sceleton 1.1

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -19,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
@@ -65,6 +68,25 @@ fun TextWithBorders(
 }
 
 @Composable
+fun PaintingWithBorders(
+    painter: Painter
+) {
+    Box(
+        modifier = Modifier
+            .padding(24.dp)
+            .border(12.dp, Color.DarkGray)
+            .background(Color.White)
+            .padding(16.dp)
+
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
 fun ArtSpaceLayout() {
     Box(
         modifier = Modifier
@@ -84,24 +106,15 @@ fun ArtSpaceLayout() {
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
         ) {
+            Spacer(modifier = Modifier.height(24.dp))
             TextWithBorders(
                 text = "Vincent van Gogh",
                 fontFamily = FontFamily(Font(R.font.permanent_marker)),
                 backColor = Color.White
                 )
-            Box(
-                modifier = Modifier
-                    .padding(24.dp)
-                    .border(12.dp, Color.DarkGray)
-                    .background(Color.White)
-                    .padding(16.dp)
-
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.vincent_van_gogh),
-                    contentDescription = null
-                )
-            }
+            PaintingWithBorders(
+                painter = painterResource(id = R.drawable.vincent_van_gogh)
+            )
             TextWithBorders(
                 text = "Self-Portrait, 1887",
                 fontFamily = FontFamily(Font(R.font.permanent_marker)),
